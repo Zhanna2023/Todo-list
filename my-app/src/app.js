f7dc4c3c25e70b3f0b56236c202aa290400a3a95
ZhannaKhizrieva <zhanna-apteka>
2024-07-30 12:10:34 +0300

    add components todo

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,64 +1,22 @@
-import { useState } from 'react';
-import {
-	useRequestAddTask,
-	useRequestUpdateTask,
-	useRequesDeleteTask,
-	useRequesGetTodos,
-} from './hooks';
+import { useEffect, useState } from 'react';
+import { Todo } from './components';
 import styles from './app.module.css';
 
 export const App = () => {
-	const [refreshTodosFlag, setRefreshTodosFlag] = useState(false);
+	const [todos, setTodos] = useState([]);
 
-	const [searchQuery, setSearchQuery] = useState('');
-
-	const refreshTodos = () => setRefreshTodosFlag(!refreshTodosFlag);
-
-	const { isLoading, todos } = useRequesGetTodos(refreshTodosFlag);
-
-	const { isCreating, requestAddTask } = useRequestAddTask(refreshTodos);
-
-	const { isUpdating, requestUpdateTask } = useRequestUpdateTask(refreshTodos);
-
-	const { isDeleting, requestDeleteTask } = useRequesDeleteTask(refreshTodos);
-
-	const handleSearchInputChange = (e) => {
-		setSearchQuery(e.target.value);
-	};
-
-	const filteredTodos = todos.filter((todo) =>
-		todo.content.toLowerCase().includes(searchQuery.toLowerCase()),
-	);
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/todos')
+			.then((todosData) => todosData.json())
+			.then((loadedTodos) => setTodos(loadedTodos));
+	}, []);
 
 	return (
 		<div className={styles.app}>
-			<div className={styles.container}>
-				<h1> Список дел</h1>
-				<input
-					type="text"
-					placeholder="Поиск..."
-					value={searchQuery}
-					onChange={handleSearchInputChange}
-					className={styles.searchInput}
-				/>
-				{isLoading ? (
-					<div className={styles.loader}></div>
-				) : (
-					filteredTodos.map(({ id, content }) => (
-						<div className={styles.container_list} key={id}>
-							{content}
-						</div>
-					))
-				)}
-				<button disabled={isCreating} onClick={requestAddTask}>
-					Добавить задание
-				</button>
-				<button disabled={isUpdating} onClick={requestUpdateTask}>
-					Обновить задание 2
-				</button>
-				<button disabled={isDeleting} onClick={requestDeleteTask}>
-					Удалить задание 3
-				</button>
+			<div>
+				{todos.map(({ id, title, completed }) => (
+					<Todo key={id} title={title} completed={completed} />
+				))}
 			</div>
 		</div>
 	);
